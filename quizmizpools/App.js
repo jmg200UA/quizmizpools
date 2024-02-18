55876215415b5b9c57f9de815dbcdb48d51b19f3
Javier Martín Gómez <j.martingomez@edu.gva.es>
2024-02-18 13:45:36 +0100

    quizmizflais

--- quizmizpools/App.js
+++ quizmizpools/App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>QuizmizPools</Text>
+      <ImageBackground
+        source={require("./assets/quizmizflais.jpeg")}
+        style={styles.backgroundImage}
+        imageStyle={styles.imageStyle}
+      >
+        <Text style={styles.centeredText}>QuizmizPools</Text>
+      </ImageBackground>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +23,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  },
+  imageStyle: {
+    opacity: 0.5,
+  },
+  centeredText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
